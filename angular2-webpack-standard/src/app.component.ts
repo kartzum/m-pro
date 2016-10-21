7bc0a8b7eca19cf0ee4654b04daf17584fa77a37
Alexander Koval <koval@netcracker.com>
2016-10-21 17:40:04 +0300

    angular2-webpack-standard. Initial.

---
+++ angular2-webpack-standard/src/app.component.ts
@@ -0,0 +1,7 @@
+import {Component} from "@angular/core";
+@Component({
+    selector: "my-app",
+    templateUrl: `<h1>Hello from Angular App with Webpack</h1>`
+})
+export class AppComponent {
+}
---
+++ angular2-webpack-standard/webpack.common.config.js
@@ -0,0 +1,41 @@
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const helpers = require("./helpers");
+
+module.exports = {
+    entry: {
+        app: "./src/main",
+        angular2: "./src/angular2.ts",
+        polyfills: "./src/polyfills.ts"
+    },
+    output: {
+        path: __dirname + "/",
+        filename: "./dist/[name].bundle.js"
+    },
+    resolve: {
+        extensions: ["", ".js", ".ts"]
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.ts$/,
+                loaders: ["awesome-typescript-loader", "angular2-template-loader"]
+            },
+            {
+                test: /\.html$/,
+                loader: "html"
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: "file?name=assets/[name].[hash].[ext]"
+            },
+            {test: /\.css$/, loader: "style!css"}
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "./dist/index.html",
+            template: "./src/index.html"
+        })
+    ]
+};
