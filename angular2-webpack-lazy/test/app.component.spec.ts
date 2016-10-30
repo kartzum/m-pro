6149457e1dfd0f611a5fb60442104954e305a2e4
kartzum <kartzum@gmail.com>
2016-10-30 12:39:51 +0300

    angular2-webpack-lazy. Test.

--- angular2-webpack-lazy/test/app.component.spec.ts
+++ angular2-webpack-lazy/test/app.component.spec.ts
@@ -1,8 +1,21 @@
-import {TestBed} from "@angular/core/testing";
-import {AppComponent} from "../src/app.component";
+import { TestBed } from "@angular/core/testing";
+import { RouterTestingModule } from "@angular/router/testing/router_testing_module";
+import { Routes } from '@angular/router';
+
+import { AppComponent } from "../src/app.component";
+
+export const fake_routes: Routes = [
+    { path: "test", component: AppComponent },
+];
+
 describe("App", () => {
     beforeEach(() => {
-        TestBed.configureTestingModule({declarations: [AppComponent]});
+        TestBed.configureTestingModule(
+            {
+                declarations: [AppComponent],
+                imports: [RouterTestingModule.withRoutes(fake_routes)
+                },
+        );
     });
     it("should work", () => {
         let fixture = TestBed.createComponent(AppComponent);
